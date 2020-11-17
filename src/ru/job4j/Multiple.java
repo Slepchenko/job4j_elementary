4eebc15f9b8de0f6b9813ce990f10e0f5f751172
Alexander Slepchenko <asander@list.ru>
2020-11-17 20:12:55 +0300

    3.3. Разбор первой программы. [#409070]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("Slepchenko Alexander");
+        System.out.println("30.03.1983");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 9 = 9");
+    }
+}

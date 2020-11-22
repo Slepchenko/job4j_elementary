6cf1925f51bf7ea345ca7907647eeb68fd1ba678
Alexander Slepchenko <asander@list.ru>
2020-11-22 21:49:02 +0300

    4.3.1. Оператор && [#409003]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,14 +1,6 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void access(int age) {
-        System.out.println("The age of the customer is : " + age);
-        if (age >= 18) {
-            System.out.println("Welcome to the cinema.");
-        } else {
-            System.out.println("It is not for you.");
-        }
-    }
 
     public static void permission(boolean allowByParent, boolean hasMoney) {
         if (allowByParent && hasMoney) {
@@ -19,8 +11,6 @@ public class Cinema {
     }
 
     public static void main(String[] args) {
-        Cinema.access(21);
-        Cinema.access(16);
 
         permission(true, true);
         permission(true, false);

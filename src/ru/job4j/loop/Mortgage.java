a1d017676c68904f9ca449e3073fbfc2e3338b61
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-01 22:01:55 +0300

    5.7. Ипотека [#409109]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,13 +4,10 @@ public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
 
-        while (true) {
+        while (amount <= 0) {
             amount = (int) ((amount * (percent / 100)) + amount - salary);
             year++;
             System.out.println(amount);
-            if (amount <= 0) {
-                break;
-            }
         }
         return year;
     }

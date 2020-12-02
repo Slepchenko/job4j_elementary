cbb2538073b6a4513128fee1f19f6384213eb5d6
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-02 14:32:57 +0300

    6.0.2. Заполнение массива. [#409031]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -11,5 +11,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        // 6.0.2
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Alexander Slepchenko";
+        names[2] = "Dima Bilan";
+        names[3] = "Kseniya Sobchak";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }

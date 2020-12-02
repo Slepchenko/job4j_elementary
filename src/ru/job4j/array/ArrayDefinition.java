767168c8d5b8d1e5bc2900cf5e778f795d12c6dd
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-02 14:22:20 +0300

    6.0.1.1. Размер массива [#409042]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+        //6.0.1
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        //6.0.1.1
+        System.out.println(ages.length);
+        System.out.println(surnames.length);
+        System.out.println(prices.length);
     }
 }

960753208215047fb6dc32cf488a79fde791029d
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-10 13:54:10 +0300

    Дополнительное задание к экзамену

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -4,14 +4,22 @@ public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
 
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
+        for (int i = 0; i < rsl.length; i++) {
+            if (left.length > 0 && right.length == 0) {
+                rsl[i] = left[i];
+            } else if (left.length == 0 && right.length > 0) {
+                rsl[i] = right[i];
+            } else {
+                if (i < left.length) {
+                    rsl[i] = left[i];
+                } else if (right[i - left.length] >= rsl[i - 1]) {
+                    rsl[i] = right[i - left.length];
+                } else {
+                    rsl[i] = rsl[i - 1];
+                    rsl[i - 1] = right[i - left.length];
+                }
+            }
         }
-
-        for (int i = left.length; i < rsl.length; i++) {
-            rsl[i] = right[i - left.length];
-        }
-
         return rsl;
     }
 }

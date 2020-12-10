b135b9ff27fedea17565ca3252f48aa57942cf0c
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-10 17:00:42 +0300

    Дополнительное задание к экзамену

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,25 +1,32 @@
 package ru.job4j.array;
 
 public class Merge {
+
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
+        int index1 = 0;
+        int index2 = 0;
 
         for (int i = 0; i < rsl.length; i++) {
-            if (left.length > 0 && right.length == 0) {
-                rsl[i] = left[i];
-            } else if (left.length == 0 && right.length > 0) {
-                rsl[i] = right[i];
-            } else {
-                if (i < left.length) {
-                    rsl[i] = left[i];
-                } else if (right[i - left.length] >= rsl[i - 1]) {
-                    rsl[i] = right[i - left.length];
-                } else {
-                    rsl[i] = rsl[i - 1];
-                    rsl[i - 1] = right[i - left.length];
+                if (index1 < left.length && index2 < right.length) {
+                    if (left[index1] < right[index2]) {
+                        rsl[i] = left[index1];
+                        index1++;
+                    } else {
+                        rsl[i] = right[index2];
+                        index2++;
+                    }
+                } else if (index1 == left.length && index2 < right.length) {
+                    rsl[i] = right[index2];
+                    index2++;
+                } else if (index1 < left.length && index2 == right.length) {
+                    rsl[i] = left[index1];
+                    index1++;
+                } else if (index1 == left.length && index2 == right.length) {
+                    break;
                 }
-            }
         }
+
         return rsl;
     }
 }

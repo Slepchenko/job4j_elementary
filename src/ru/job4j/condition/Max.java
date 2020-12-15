dd4bdc0bee37445c6279b581e67e720db5a0c501
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-15 17:50:25 +0300

    3. Перегрузить метод max для трех чисел [#409209]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,8 +3,17 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
         boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
+        return condition ? left : right;
+    }
+
+    public static int max(int left, int right, int bottom) {
+        boolean condition = max(left, right) > bottom;
+        return condition ? max(left, right) : bottom;
+    }
+
+    public static int max(int left, int right, int top, int bottom) {
+        boolean condition = max(left, right, top) > bottom;
+        return condition ? max(left, right, top) : bottom;
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -24,4 +24,47 @@ public class MaxTest {
         int result = Max.max(2, 2);
         assertThat(result, is(2));
     }
+
+    // 3. Перегрузить метод max для трех чисел [#409209]
+    @Test
+    public void whenMax2Left3Top1Then2() {
+        int result = Max.max(2, 3, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax5Left2Top1Then5() {
+        int result = Max.max(5, 2, 2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax2Left2Top2Then2() {
+        int result = Max.max(2, 2, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax2Left3Top1Bottom6Then6() {
+        int result = Max.max(2, 3, 1, 6);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenMax6Left3Top7Bottom6Then7() {
+        int result = Max.max(6, 3, 7, 6);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax2Left3Top2Bottom3Then3() {
+        int result = Max.max(2, 3, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax2Left2Top2Bottom2Then2() {
+        int result = Max.max(2, 2, 2, 2);
+        assertThat(result, is(2));
+    }
 }

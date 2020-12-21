f3037aa7aedee5b0accac6e85cdfe0fcfe32b656
Alexander Slepchenko <asander@list.ru>
2020-12-21 20:53:21 +0300

    3. Интерфейс Input [#409161]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,28 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void move() {
+        System.out.println("Еду");
+    }
+
+    @Override
+    public void passengers(int passenger) {
+        System.out.println("Колличество пассажиров" + passenger);
+    }
+
+    @Override
+    public double tuck(String fuel) {
+        int value = 20;
+        double petrolAI92 = 52.5;
+        double petrolAI98 = 55.8;
+        double dt = 57.4;
+        if (fuel.endsWith("92")) {
+            return petrolAI92 * value;
+        } else if (fuel.equals("98")) {
+            return petrolAI98 * value;
+        } else {
+            return dt * value;
+        }
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void move();
+
+    void passengers(int passenger);
+
+    double tuck(String fuel);
+}

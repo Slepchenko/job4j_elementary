e0a010bab8c1a2256d4b1375965cbde8349031c3
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-25 21:05:56 +0300

    0.4. Пользовательские исключения. [#409181]

---
+++ src/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+
+        for(int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            } else {
+                throw new ElementNotFoundException();
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = new String[]{"one", "two", "three", "five"};
+        int rsl = -1;
+        try {
+            rsl = indexOf(value, "four");
+        } catch(ElementNotFoundException e) {
+            System.out.println("Нет такого значения");
+        }
+    }
+}
